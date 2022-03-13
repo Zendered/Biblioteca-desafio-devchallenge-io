68d5b45dbb8bd76699f5bd95cc931d26280407a2
Luiz <luizz.ggr@gmail.com>
2022-03-13 00:54:37 -0300

    feat: book entity

--- src/domain/entities/book.ts
+++ src/domain/entities/book.ts
@@ -1,30 +1,21 @@
-import { randomUUID } from 'crypto';
 import { Either, left, right } from '@/shared';
+import { randomUUID } from 'crypto';
 import { IBooksDTO } from '../contracts/gateways';
-import { Author } from './author';
-import { InvalidAuthor, InvalidID } from './error';
-import { _ID } from './_id';
+import { Entity } from './entity';
+import { InvalidEntity } from './error/invalid-entity';
 
 export class Book {
-  constructor(readonly id: _ID, readonly author: Author, readonly editor: string, readonly thumbnail: string, readonly title: string) {
-    this.id = id ?? randomUUID();
+  constructor(readonly id: string, readonly author: String, readonly editor: string, readonly thumbnail: string, readonly title: string) {
+    this.id = randomUUID();
   }
 
-  static create(book: IBooksDTO): Either<InvalidAuthor | InvalidID, Book> {
-    const authorOrError = Author.create(book.author);
+  static create(book: IBooksDTO): Either<InvalidEntity, Book> {
+    const bookOrError = Entity.create(book);
 
-    if (authorOrError.isLeft()) {
-      return left(new InvalidAuthor(book.author));
+    if (bookOrError.isLeft()) {
+      return left(new InvalidEntity());
     }
 
-    const idOrError = _ID.create(book.id);
-    if (idOrError.isLeft()) {
-      return left(new InvalidID());
-    }
-
-    const author: Author = authorOrError.value as Author;
-    const id: _ID = idOrError.value as _ID;
-
-    return right(new Book(id, author, book.editor, book.thumbnail, book.title));
+    return right(new Book(book.id as string, book.author, book.editor, book.thumbnail, book.title));
   }
 }
