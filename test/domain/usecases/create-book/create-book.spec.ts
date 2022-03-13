d162593c00247fc9ebd419a7bdbf48c83effb498
Luiz <luizz.ggr@gmail.com>
2022-03-13 00:59:27 -0300

    test: create book usecase

---
+++ test/domain/usecases/create-book/create-book.spec.ts
@@ -0,0 +1,21 @@
+import { IBooksDTO } from '@/domain/contracts/gateways';
+import { IBookRepository } from '@/domain/contracts/gateways/book-repository';
+import { BookRepository } from '@/domain/contracts/repository/in-memory-book-repository';
+import { Book } from '@/domain/entities';
+import { CreateBook } from '@/domain/usecases/create-book/create-book';
+
+describe('Create category usecase ', () => {
+  const books: IBooksDTO[] = [];
+  const bookRepository: IBookRepository = new BookRepository(books);
+  const usecase: CreateBook = new CreateBook(bookRepository);
+  test('should create a book with valid data', async () => {
+    const validBook:IBooksDTO = {
+      author: 'author01',
+      editor: 'editor01',
+      thumbnail: 'thumbnail01',
+      title: 'title01',
+    };
+    const result = await (await usecase.perform(validBook)).value as Book;
+    expect(result).toEqual(validBook);
+  });
+});
