6ac5d40355fa12389d74930ec6324c971c888436
Luiz <luizz.ggr@gmail.com>
2022-03-13 00:59:08 -0300

    test: book entity

--- test/domain/entities/book.spec.ts
+++ test/domain/entities/book.spec.ts
@@ -12,11 +12,10 @@ describe('Book entity', () => {
       title: 'title01',
     };
     const response = Book.create(book).value as Book;
-    console.log(response);
-    expect(response.author.value).toBe('author01');
+    expect(response.author).toBe('author01');
   });
 
-  test('should not add a book without a author', () => {
+  test('should not add a book with null author', () => {
     const book: IBooksDTO = {
       author: '',
       editor: 'editor01',
@@ -24,7 +23,43 @@ describe('Book entity', () => {
       title: 'title01',
     };
     const response = Book.create(book).value as Error;
-    expect(response.name).toEqual('InvalidAuthor');
-    expect(response.message).toEqual(`Invalid Author: ${book.author}.`);
+    expect(response.name).toEqual('InvalidEntity');
+    expect(response.message).toEqual('Something went wrong.');
+  });
+
+  test('should not add a book with null editor', () => {
+    const book: IBooksDTO = {
+      author: 'author01',
+      editor: '',
+      thumbnail: 'link01',
+      title: 'title01',
+    };
+    const response = Book.create(book).value as Error;
+    expect(response.name).toEqual('InvalidEntity');
+    expect(response.message).toEqual('Something went wrong.');
+  });
+
+  test('should not add a book with null thumbnail', () => {
+    const book: IBooksDTO = {
+      author: 'author01',
+      editor: 'editor01',
+      thumbnail: '',
+      title: 'title01',
+    };
+    const response = Book.create(book).value as Error;
+    expect(response.name).toEqual('InvalidEntity');
+    expect(response.message).toEqual('Something went wrong.');
+  });
+
+  test('should not add a book with null title', () => {
+    const book: IBooksDTO = {
+      author: 'author01',
+      editor: 'editor01',
+      thumbnail: 'thumbnail01',
+      title: '',
+    };
+    const response = Book.create(book).value as Error;
+    expect(response.name).toEqual('InvalidEntity');
+    expect(response.message).toEqual('Something went wrong.');
   });
 });
