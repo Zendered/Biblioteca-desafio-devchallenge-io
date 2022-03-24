23e55ed018a9699058880d76c55f6d0273c27a37
Luiz <luizz.ggr@gmail.com>
2022-03-23 21:35:42 -0300

    feat: enabling CORS middleware

--- src/main/config/middleware.ts
+++ src/main/config/middleware.ts
@@ -1,6 +1,8 @@
 import { Express } from 'express';
 import { BodyParser } from '../middleware/body-parser';
+import { cors } from '../middleware/cors';
 
 export const setupMiddleware = (app: Express): void => {
   app.use(BodyParser);
+  app.use(cors);
 };
